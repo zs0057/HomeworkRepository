fe7bf4501eb113abca51d13532dd32f9523950b7
지성 <soss0527@naver.com>
2021-09-24 14:57:33 +0000

    HTML: data-link, JS: scrollTo

---
+++ main.js
@@ -0,0 +1,6 @@
+// Handle scrolling when tapping on the navbar menu 
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', () => {
+	const target = event.target;
+	const link = target.dataset.link;
+})
