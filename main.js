e58705ab144e80fbd4591d8116594bf2861a9efc
지성 <soss0527@naver.com>
2021-09-26 13:56:22 +0000

    JS: Navbar Transparent CSS: Navbar--dark

--- main.js
+++ main.js
@@ -3,4 +3,17 @@ const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', () => {
 	const target = event.target;
 	const link = target.dataset.link;
-})
+	const scrollTo = document.querySelector(`${link}`);
+	scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+	if (window.scrollY > navbarHeight) {
+		navbar.classList.add('navbar--dark');
+	} else {
+		navbar.classList.remove('navbar--dark');
+	}
+});
